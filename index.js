f2ad577925706ef21343739a674895ae3bcd81a8
ChoiWonPyo <choiwp626@gmail.com>
2018-05-23 22:32:38 +0900

    22

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get('/api/posts/:years/:month', (req,res) => {
 	res.send(req.query);
 })
 // /api/courses/1
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
